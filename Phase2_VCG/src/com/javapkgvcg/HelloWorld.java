37d4c39447136276b7a43f3b44cb665197788e2c
pavan <pavan_gunji@epam.com>
2022-02-07 20:41:09 +0530

    conflict message

--- Phase2_VCG/src/com/javapkgvcg/HelloWorld.java
+++ Phase2_VCG/src/com/javapkgvcg/HelloWorld.java
@@ -5,7 +5,12 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Hello Programmer");
+
 		System.out.println("Hello Reviewer");
 		System.out.println("1 commit in master - Rebase commit message 1");
+
+		System.out.println("add 2 additional commits to test - Rebase Commit 1");
+		
+
 	}
 }
