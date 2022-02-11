a7984b76cdcbfb7f6a1524fe731fc964f15ad1fa
pavan <pavan_gunji@epam.com>
2022-02-12 01:08:23 +0530

    Commit cherry-picked changes for revert operation

--- Phase2_VCG/src/com/javapkgvcg/CherryPick.java
+++ Phase2_VCG/src/com/javapkgvcg/CherryPick.java
@@ -3,6 +3,5 @@ package com.javapkgvcg;
 public class CherryPick {
 public static void main(String[] args) {
 	System.out.println("This is a Cherry-Pick challenge");
-	System.out.println("This is a Revert Challenge");
 }
 }
