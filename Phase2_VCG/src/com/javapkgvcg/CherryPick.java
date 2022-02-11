bd25b4655c305ba66ef98e0a39836130c5e51be9
'pavan <'pavan_gunji@epam.com'>
2022-02-12 01:36:03 +0530

    CP

--- Phase2_VCG/src/com/javapkgvcg/CherryPick.java
+++ Phase2_VCG/src/com/javapkgvcg/CherryPick.java
@@ -3,5 +3,6 @@ package com.javapkgvcg;
 public class CherryPick {
 public static void main(String[] args) {
 	System.out.println("This is a Cherry-Pick challenge");
+	System.out.println("CP");
 }
 }
