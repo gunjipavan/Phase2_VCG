4e613407a3fbe5e522c796c5a33e97b9c7449232
'pavan <'pavan_gunji@epam.com'>
2022-02-12 01:36:46 +0530

    Revert

--- Phase2_VCG/src/com/javapkgvcg/CherryPick.java
+++ Phase2_VCG/src/com/javapkgvcg/CherryPick.java
@@ -4,5 +4,6 @@ public class CherryPick {
 public static void main(String[] args) {
 	System.out.println("This is a Cherry-Pick challenge");
 	System.out.println("CP");
+	System.out.println("Revert");
 }
 }
