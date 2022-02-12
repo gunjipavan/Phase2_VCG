fd3ff38547e182951396721fed6d404bf5c85f88
pavan <pavan_gunji@epam.com>
2022-02-12 16:47:34 +0530

    CP - Cherry Picking

--- Phase2_VCG/src/com/javapkgvcg/CherryPick.java
+++ Phase2_VCG/src/com/javapkgvcg/CherryPick.java
@@ -3,7 +3,7 @@ package com.javapkgvcg;
 public class CherryPick {
 public static void main(String[] args) {
 	System.out.println("This is a Cherry-Pick challenge");
-	System.out.println("CP");
+	System.out.println("CP - Cherry Picking");
 
 	System.out.println("Revert");
 
